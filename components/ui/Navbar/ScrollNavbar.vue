3bd749fd0ea10216c5b399c020f0cd063510ad25
KaioRodrigoDev <kaiorodrigocoder@gmail.com>
2023-05-05 20:56:38 -0300

    Creating NavBar

---
+++ components/ui/Navbar/ScrollNavbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="fixed top-0 left-0 z-50 w-full bg-[#F1F4F9]">
+    <div class="max-w-full px-4 mx-auto sm:px-6 lg:px-8">
+      <div class="flex h-16">
+        <div class="flex justify-between w-full">
+          <div class="flex items-center flex-shrink-0">
+            <img
+              src="@/assets/images/logo/logo-lqdi-positivo-1.png"
+              href="logo"
+            />
+          </div>
+          <div class="items-center hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+            <p>Agende uma reunião conosco</p>
+            <a
+              class="flex items-center justify-center h-12 px-6 text-center text-white bg-black rounded-full"
+              href=""
+              >Começar</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
