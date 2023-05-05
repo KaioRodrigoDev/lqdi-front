51d2691170bf8ee9af15ba19af0eab866b52c882
KaioRodrigoDev <kaiorodrigocoder@gmail.com>
2023-05-05 20:57:33 -0300

    Creating Banner Component

---
+++ components/home/banner/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <section>
+    <div class="flex bg-[#F1F4F9] px-6 sm:px-8 lg:px-10">
+      <div class="w-1/3 mt-16">
+        <h1 class="text-[#08111E] font-semibold font-body text-4xl">
+          Construímos estratégias, design e tecnologia de ponta para o seu
+          negócio
+        </h1>
+        <p class="text-[#08111E] font-semibold font-body text-xl mt-8">
+          Converse com nossa equipe sobre o seu projeto.
+        </p>
+        <div class="w-32 mt-8">
+          <a
+            class="flex items-center justify-center h-12 px-6 text-center text-white bg-[#0AB2B7] rounded-full"
+            href=""
+            >Começar</a
+          >
+        </div>
+      </div>
+      <div class="flex">
+        <img
+          src="@/assets/images/home/banner/Rectangle-112.png"
+          class="self-end mr-4 h-fit"
+          href="img1"
+        />
+        <div class="flex-col">
+          <img
+            src="@/assets/images/home/banner/Rectangle-109.png"
+            href="img1"
+          />
+          <img
+            class="mt-4 mr-4"
+            src="@/assets/images/home/banner/Rectangle-111.png"
+            href="img1"
+          />
+        </div>
+        <div class="mt-16">
+          <img
+            src="@/assets/images/home/banner/Rectangle-110.png"
+            href="img1"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
