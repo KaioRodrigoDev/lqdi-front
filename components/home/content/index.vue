cc4c6180d3e31c8de2914670a19db15df011a299
KaioRodrigoDev <kaiorodrigocoder@gmail.com>
2023-05-05 20:57:45 -0300

    Creating Content Component

---
+++ components/home/content/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="-mt-24 bg-white">
+    <div class="flex px-6 pt-64 sm:px-8 lg:px-10">
+      <div class="flex">
+        <div class="flex-col max-w-md mr-8">
+          <img
+            src="@/assets/images/home/content/Rectangle-113.png"
+            href="img1"
+            class="h-96"
+          />
+          <div class="max-w-fit">
+            <p class="text-2xl font-semibold font-body">
+              Por que é importante ter um site de qualidade?
+            </p>
+            <p class="mt-4 font-normal font-body">
+              Um site de qualidade passou a ser um dos meios de exposição de
+              marca mais eficientes no ambiente online. Ele funciona como uma
+              espécie de cartão...
+            </p>
+          </div>
+          <div class="mt-8">
+            <a class="font-bold font-body" href="">Ir para a pagina!</a>
+          </div>
+        </div>
+        <div class="max-w-md mr-8">
+          <img
+            src="@/assets/images/home/content/Rectangle-114.png"
+            href="img1"
+            class="h-96"
+          />
+          <p class="text-2xl font-semibold font-body">
+            6 dicas para trabalhar com marketing digital com verba limitada
+          </p>
+          <p class="mt-4 font-normal font-body">
+            Um site de qualidade passou a ser um dos meios de exposição de marca
+            mais eficientes no ambiente online. Ele funciona como uma espécie de
+            cartão...
+          </p>
+          <div class="mt-8">
+            <a class="font-bold font-body" href="">Ir para a pagina!</a>
+          </div>
+        </div>
+        <div class="flex-col w-full p-12 text-white bg-black rounded-lg">
+          <div class="pr-8">
+            <p class="text-5xl font-medium font-body">
+              Receba os nossos artigo de interesse na sua caixa de entrada.
+            </p>
+          </div>
+
+          <input
+            class="items-center p-4 mt-12 text-lg text-center bg-black border-2 rounded-full"
+            placeholder="Escreva aqui seu e-mail..."
+          />
+
+          <div class="mt-12 w-fit">
+            <div class="w-24">
+              <p class="text-sm">Siga-nos em nossas mídias sociais</p>
+            </div>
+
+            <div class="flex mt-12 space-x-4">
+              <div class="p-4 bg-white rounded-full w-fit h-fit">
+                <img
+                  src="@/assets/images/home/content/Vector.png"
+                  href="img1"
+                />
+              </div>
+              <div class="p-4 bg-white rounded-full w-fit h-fit">
+                <img
+                  src="@/assets/images/home/content/feFacebook2.png"
+                  href="img1"
+                />
+              </div>
+              <div class="p-4 bg-white rounded-full w-fit h-fit">
+                <img
+                  src="@/assets/images/home/content/VectorIn.png"
+                  href="img1"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
