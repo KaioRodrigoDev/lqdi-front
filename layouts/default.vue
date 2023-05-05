880db6e1d6c3b4ad1f48f644f160bd8b62b0cdd8
KaioRodrigoDev <kaiorodrigocoder@gmail.com>
2023-05-05 20:57:15 -0300

    Creating Layout

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ScrollNavbar />
+    <section class="pt-16 pb-16">
+      <slot />
+    </section>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import ScrollNavbar from '@/components/ui/Navbar/ScrollNavbar.vue'
+import Footer from '~/components/ui/Footer/Footer.vue'
+
+export default {
+  components: {
+    ScrollNavbar,
+    Footer
+  }
+}
+</script>
