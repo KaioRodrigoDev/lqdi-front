29c8e70abb1a5c4e9acd7a76b4bc8d66a5ab6802
KaioRodrigoDev <kaiorodrigocoder@gmail.com>
2023-05-05 20:58:25 -0300

    Creating Home Page

--- pages/home.vue
+++ pages/home.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <h1 class="text-red-500">Hello</h1>
+    <Banner />
+    <Content />
+    <Form />
   </div>
 </template>
+
+<script>
+import Banner from '@/components/home/banner'
+import Content from '@/components/home/content'
+import Form from '@/components/home/formSection'
+
+export default {
+  components: {
+    Banner,
+    Content,
+    Form
+  }
+}
+</script>
