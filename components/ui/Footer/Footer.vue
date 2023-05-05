c3e36d8d9512c247f4358102b3b0fa2aa2229aac
KaioRodrigoDev <kaiorodrigocoder@gmail.com>
2023-05-05 20:56:57 -0300

    Creating Footer

---
+++ components/ui/Footer/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="w-full py-12 text-white bg-black">
+    <div class="max-w-full px-4 mx-auto sm:px-6 lg:px-8">
+      <div class="flex">
+        <div class="flex justify-between w-full">
+          <div class="mt-4">
+            <img
+              src="@/assets/images/logo/logo-lqdi-positivo-2.png"
+              href="logo"
+            />
+          </div>
+          <div class="flex mt-4 mr-32">
+            <div class="items-center mr-12">
+              <p class="font-semibold font-body">Brasil</p>
+              <p class="font-light font-body">
+                Rua da Consolação, 4393º andar, Consolação São Paulo
+              </p>
+              <br />
+              <p class="font-light font-body">+55 11 3280-0283</p>
+            </div>
+            <div>
+              <div class="items-center mb-4">
+                <p class="font-semibold font-body">Portugal</p>
+                <p class="font-light font-body">
+                  Av. República Nº6 - 1º Andar Lisboa
+                </p>
+                <br />
+                <p class="font-light font-body">+351 916 029 443</p>
+              </div>
+              <div class="items-center">
+                <p class="font-semibold font-body">Holanda</p>
+                <p class="font-light font-body">
+                  Jasykoffstraat 631506AT ZaandamThe Netherlands
+                </p>
+                <br />
+                <p class="font-light font-body">+31 622 333 744</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
